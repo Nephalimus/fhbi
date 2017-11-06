46de93c849e6da285d776ba154f141a21495553a
Oliver Trappe <Oliver Trappe@Megatron.diplexa.de>
2017-11-06 15:54:59 +0100

    alternativ

--- otrappe/src/otrappe/praktikum2_3c_alt.java
+++ otrappe/src/otrappe/praktikum2_3c_alt.java
@@ -3,15 +3,16 @@ package otrappe;
 import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
-public class praktikum2_3c {
+public class praktikum2_3c_alt {
 
 	public static void main(String[] args) {
-		int anzahl =0;
-		do {
+		StdOut.print("Bitte geben Sie die Anzahl der Exponenten ein: ");
+		int anzahl = StdIn.readInt();
+		while(anzahl<1 || anzahl>30){
+			StdOut.println("Der von Ihnen eingegebene Exponent liegt nicht im zulässigen Bereich 1-30!");		
 			StdOut.print("Bitte geben Sie die Anzahl der Exponenten neu ein: ");
 			anzahl = StdIn.readInt();
-		} while(anzahl<1 || anzahl>30);
-		
+		} 
 		int sum = 1;
 		for(int i=1;i<=anzahl;i++){
 			sum *= 2;
