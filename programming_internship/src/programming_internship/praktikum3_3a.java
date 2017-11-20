de8248b08ae8c13b581500ccc3dc1617b58e0857
Nephalimus <33394826+Nephalimus@users.noreply.github.com>
2017-11-20 13:49:17 +0100

    Update praktikum3_3a.java

--- programming_internship/src/programming_internship/praktikum3_3a.java
+++ programming_internship/src/programming_internship/praktikum3_3a.java
@@ -6,16 +6,16 @@ import edu.princeton.cs.introcs.StdOut;
 public class praktikum3_3a {
 
 	public static void main(String[] args) {
-		StdOut.println("Dieses Programm nutzt eine Methode zur Abfrage einer Zahl mit Parameter");
+		StdOut.println("Dieses Programm nutzt eine Methode zur Abfrage einer Zahl mit Parameter (Fragetext)");
 		System.out.println();
 		
+		//Die nächsten 4 Zeilen könnten auch ersetzt werden durch   String eingabe = "<FRAGETEXT>";
 		StdOut.print("Bitte den Fragetext eingeben:");
 		String eingabe = StdIn.readLine();
-			
 		System.out.println();
 		System.out.println("Ausgabe: ");
 		
-		int ausgabe = abfrage(eingabe);
+		int ausgabe = abfrage(eingabe);	//Methodenaufruf
 
 		System.out.println();
 		System.out.println("Die eingegebene Zahl ist: " + ausgabe);
