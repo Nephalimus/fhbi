285a049dbed081fddd7ce55fb79353c639e8416e
otrappe <otrappe@DBNHG52.ad.fh-bielefeld.de>
2018-06-27 14:56:39 +0200

    BMI

--- programming_internship/src/programming_internship/BMI.java
+++ programming_internship/src/programming_internship/BMI.java
@@ -9,20 +9,20 @@ import javax.swing.*;
 
 
 public class BMI extends JFrame{
-	
+	private static JFrame currentwindow;
 	private JLabel lblGewicht = new JLabel("Gewicht:  ");
 	private JLabel lblGroesse = new JLabel("Größe:  ");
 	private JLabel lblBmi = new JLabel("BMI:  ");
 	private static JTextField tfGewicht = new JTextField();
 	private static JTextField tfGroesse = new JTextField();
 	private static JTextField tfBmi = new JTextField();
-	private JPanel pWest = new JPanel(new GridLayout(3,2,5,5));
-	private JPanel pCenter = new JPanel(new GridLayout(3,2,5,5));
+	private JPanel pWest = new JPanel(new GridLayout(3,1,20,5));
+	private JPanel pCenter = new JPanel(new GridLayout(3,1,20,5));
 	private JPanel pSouth = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 	private static JButton btnCalculate = new JButton("Rechne!");
 
 	public BMI() {
-		
+		currentwindow = this;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(100, 100);
 		setTitle("BMI");
@@ -38,9 +38,9 @@ public class BMI extends JFrame{
 		pCenter.add(tfGewicht);
 		pCenter.add(tfGroesse);
 		pCenter.add(tfBmi);
-		tfGewicht.setPreferredSize(new Dimension(150,20));
-		tfGroesse.setPreferredSize(new Dimension(150,20));
-		tfBmi.setPreferredSize(new Dimension(150,20));
+		tfGewicht.setPreferredSize(new Dimension(150,30));
+		tfGroesse.setPreferredSize(new Dimension(150,30));
+		tfBmi.setPreferredSize(new Dimension(150,30));
 		tfBmi.setEditable(false);
 		
 		pSouth.add(this.btnCalculate);
@@ -56,31 +56,41 @@ public class BMI extends JFrame{
 		new BMI();
 
 		btnCalculate.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent arg0){
-				double gewicht = 0;
-				double groesse = 0;
+			public void actionPerformed(ActionEvent arg0){			
+				tfGewicht.setText(tfGewicht.getText().replace(",","."));
+				double gewicht = pruefen(tfGewicht.getText(),tfGewicht);
 				
-				try{
-					gewicht = new Double(tfGewicht.getText()).doubleValue();
-				}catch(NumberFormatException e){
-					tfBmi.setText("");
-				}
-				
+				tfGroesse.setText(tfGroesse.getText().replace(",","."));
+				double groesse = pruefen(tfGroesse.getText(),tfGroesse);
+	
+				double bmi;
 				try {
-					groesse = new Double(tfGroesse.getText()).doubleValue();
-				} catch (NumberFormatException e) {
-					tfBmi.setText("");
+					bmi = gewicht/(groesse*groesse);
+					DecimalFormat df = new DecimalFormat("#.##");
+					String ausgabe = df.format(bmi);
+					tfBmi.setText(ausgabe);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				double bmi = gewicht/(groesse*groesse);
-				DecimalFormat df = new DecimalFormat("#.##");
-				String ausgabe = df.format(bmi);
-				tfBmi.setText(ausgabe);
-				
 			}
 
 		
 		});
 		
+	}
+	
+
+	public static double pruefen(String wert, JTextField feldn) {
+		double dwert;
+		try{
+			dwert =  new Double(wert).doubleValue();
+		}catch(NumberFormatException e){
+			JOptionPane.showMessageDialog(currentwindow, "Eingabefehler - Zahl erwartet");
+			dwert=0;
+			feldn.setText("0");
+		}
+		return dwert;
 	}
 
 }
